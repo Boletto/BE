83d6d06d0af882c1822726b5285ca3f84ab84a24
Eeap <kdg97811@naver.com>
2024-09-15 10:53:07 +0900

    feat: get sticker list of travel api

--- src/main/java/com/demoboletto/controller/StickerController.java
+++ src/main/java/com/demoboletto/controller/StickerController.java
@@ -1,13 +1,24 @@
 package com.demoboletto.controller;
 
+import com.demoboletto.dto.global.ResponseDto;
+import com.demoboletto.service.StickerService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @Tag(name="Sticker", description = "스티커 관련 API")
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/travel/sticker")
 public class StickerController {
+    private final StickerService stickerService;
+    @GetMapping("/")
+    @Operation(summary = "get sticker list", description = "Get all sticker list in the travel.")
+    public ResponseDto<?> getStickerList(@RequestParam(value = "travel_id") Long id) {
+        return ResponseDto.ok(stickerService.getStickersByTravelId(id));
+    }
 }
