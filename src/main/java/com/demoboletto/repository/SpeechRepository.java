df5a8d9f7d63c05a84f62db08d82327969be2049
Eeap <kdg97811@naver.com>
2024-09-17 19:25:10 +0900

    fix: fix delete query error

--- src/main/java/com/demoboletto/repository/SpeechRepository.java
+++ src/main/java/com/demoboletto/repository/SpeechRepository.java
@@ -2,6 +2,7 @@ package com.demoboletto.repository;
 
 import com.demoboletto.domain.Speech;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +12,7 @@ import java.util.List;
 public interface SpeechRepository extends JpaRepository<Speech, Long> {
     @Query("select s from Speech s where s.travel.travelId = :id")
     List<Speech> findAllByTravelId(Long id);
+    @Modifying
     @Query("delete from Speech s where s.travel.travelId = :travelId")
     void deleteAllByTravelId(Long travelId);
 
