7dc75982f8046d91fd48396d2676c8666bd33507
leeeunda <daun0219@gmail.com>
2024-09-22 00:45:09 +0900

    feat: separate kakao & apple login

--- src/main/java/com/demoboletto/service/KakaoService.java
+++ src/main/java/com/demoboletto/service/KakaoService.java
@@ -1,61 +1,31 @@
 package com.demoboletto.service;
 
-import com.demoboletto.dto.request.AuthSignUpDto;
+import com.demoboletto.dto.request.OauthLoginDto;
 import com.demoboletto.dto.response.JwtTokenDto;
 import com.demoboletto.exception.CommonException;
 import com.demoboletto.exception.ErrorCode;
 import com.demoboletto.repository.UserRepository;
-import com.demoboletto.utility.JwtUtil;
-import com.demoboletto.dto.request.OauthLoginDto;
 import com.demoboletto.domain.User;
+import com.demoboletto.type.EProvider;
 import com.demoboletto.type.ERole;
+import com.demoboletto.utility.JwtUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-@Service
+@Slf4j
 @RequiredArgsConstructor
-public class AuthService {
+@Service
+public class KakaoService {
+
     private final JwtUtil jwtUtil;
     private final UserRepository userRepository;
 
-    public boolean checkDuplicate(String email) {
-        return userRepository.existsBySerialId(email);
-    }
-
-//    public void signUp(AuthSignUpDto authSignUpDto) {
-//        userRepository.save(
-//                User.signUp(authSignUpDto, bCryptPasswordEncoder.encode(authSignUpDto.password()))
-//        );
-//    }
-
-    @Transactional
-    public JwtTokenDto login(OauthLoginDto userLoginDto) {
-        User user;
-        boolean isNewUser = false;
-
-        Optional<User> existingUser = userRepository.findBySerialId(userLoginDto.serialId());
-
-        if (existingUser.isPresent()) {
-            user = existingUser.get();
-        } else {
-            // 새로운 사용자인 경우 회원 정보 저장
-            user = userRepository.save(User.signUp(userLoginDto.serialId(), userLoginDto.provider(), userLoginDto.nickname()));
-            isNewUser = true;
-        }
-
-        //JWT 토큰 생성
-        JwtTokenDto jwtTokenDto = jwtUtil.generateTokens(user.getId(), ERole.USER);
-
-        // 신규 사용자이거나 리프레시 토큰이 변경된 경우 갱신
-        if (isNewUser || !jwtTokenDto.refreshToken().equals(user.getRefreshToken())) {
-            userRepository.updateRefreshTokenAndLoginStatus(user.getId(), jwtTokenDto.refreshToken(), true);
-        }
-
-        return jwtTokenDto;
+    public EProvider getProvider() {
+        return EProvider.KAKAO;
     }
 
     @Transactional
@@ -68,4 +38,33 @@ public class AuthService {
 
         return jwtTokenDto;
     }
+
+    @Transactional
+    public JwtTokenDto login(OauthLoginDto userLoginDto) {
+        User user;
+        boolean isNewUser = false;
+
+        Optional<User> existingUser = userRepository.findBySerialId(userLoginDto.serialId());
+
+        if (existingUser.isPresent()) {
+            user = existingUser.get();
+        } else {
+            user = userRepository.save(User.signUp(userLoginDto.serialId(), userLoginDto.provider(), userLoginDto.nickname()));
+            isNewUser = true;
+        }
+
+        JwtTokenDto jwtTokenDto = jwtUtil.generateTokens(user.getId(), ERole.USER);
+
+        if (isNewUser || !jwtTokenDto.refreshToken().equals(user.getRefreshToken())) {
+            userRepository.updateRefreshTokenAndLoginStatus(user.getId(), jwtTokenDto.refreshToken(), true);
+        }
+
+        return jwtTokenDto;
+    }
+
+    public boolean checkDuplicate(String email) {
+        return userRepository.existsBySerialId(email);
+    }
+
+
 }
