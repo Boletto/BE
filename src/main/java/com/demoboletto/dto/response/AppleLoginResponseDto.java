72d140bf4cc935c6f774f66e4083daba236331cb
leeeunda <daun0219@gmail.com>
2024-09-29 16:05:07 +0900

    fix: login dto

---
+++ src/main/java/com/demoboletto/dto/response/AppleLoginResponseDto.java
@@ -0,0 +1,30 @@
+package com.demoboletto.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+@Builder
+@Schema(description = "Apple Login Response Dto")
+public record AppleLoginResponseDto(
+        @JsonProperty("accessToken")
+        @Schema(description = "액세스 토큰")
+        String accessToken,
+
+        @JsonProperty("refreshToken")
+        @Schema(description = "리프레시 토큰")
+        String refreshToken,
+
+        @JsonProperty("userName")
+        @Schema(description = "유저의 이름", example = "이다은")
+        String name,
+
+        @JsonProperty("userNickName")
+        @Schema(description = "유저의 닉네임", example = "leeeunda")
+        String nickname,
+
+        @JsonProperty("userProfile")
+        @Schema(description = "유저의 프로필 이미지 url", example = "https://boletto.s3.ap-northeast-2.amazonaws.com/스크린샷 2023-12-12 오후 12.49.20.png")
+        String userProfile
+) {
+}
--- src/main/java/com/demoboletto/dto/response/JwtTokenDto.java
+++ src/main/java/com/demoboletto/dto/response/JwtTokenDto.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
+import lombok.Getter;
 
 import java.io.Serializable;
+
 
 @Builder
 @Schema(name = "JwtTokenDto", description = "JWT 토큰 응답")
