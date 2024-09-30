9ec34aa0d34af560b31944814dffb56e53c9af94
leeeunda <daun0219@gmail.com>
2024-09-30 13:03:53 +0900

    feat: add domain alarm setting

---
+++ src/main/java/com/demoboletto/domain/AlarmSettings.java
@@ -0,0 +1,54 @@
+package com.demoboletto.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.joda.time.DateTime;
+import org.joda.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@Table(name = "alarm_settings")
+public class AlarmSettings {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "settings_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "all_notifications")
+    private Boolean allNotifications;
+
+    @Column(name = "sticker_frame_notifications")
+    private Boolean stickerFrameNotifications;
+
+    @Column(name = "friend_request_notifications")
+    private Boolean friendRequestNotifications;
+
+    @Column(name = "travel_invitation_notifications")
+    private Boolean travelInvitationNotifications;
+
+    @Column(name = "location_sharing_consent")
+    private Boolean locationSharingConsent;
+
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+
+    @Builder
+    public AlarmSettings(User user, Boolean allNotifications,
+                                Boolean stickerFrameNotifications, Boolean friendRequestNotifications,
+                                Boolean travelInvitationNotifications, Boolean locationSharingConsent) {
+        this.user = user;
+        this.allNotifications = allNotifications;
+        this.stickerFrameNotifications = stickerFrameNotifications;
+        this.friendRequestNotifications = friendRequestNotifications;
+        this.travelInvitationNotifications = travelInvitationNotifications;
+        this.locationSharingConsent = locationSharingConsent;
+        this.createdDate = new LocalDateTime();
+    }
+}
