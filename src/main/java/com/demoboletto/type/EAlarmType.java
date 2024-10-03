5e2f87268944d0ce2b5aee72a849138caf1f2c74
leeeunda <daun0219@gmail.com>
2024-10-04 08:33:50 +0900

    fix: alarm message

--- src/main/java/com/demoboletto/type/EAlarmType.java
+++ src/main/java/com/demoboletto/type/EAlarmType.java
@@ -9,7 +9,7 @@ public enum EAlarmType {
     STICKER_ACQUISITION("{stickerName} 스티커를 획득했어요."),
     REGION_ACTIVATION("'{regionName}' 네컷 제작이 활성화되었어요."),
     REGION_DEACTIVATION("'{regionName}' 네컷 제작이 비활성화되었어요."),
-    TRAVEL_TICKET("초대받은 여행 티켓이 {ticketCount}개 있어요."),
+    TRAVEL_TICKET("여행 티켓이 도착했어요!"),
     FRIEND_INVITE_SENT("친구에게 초대를 전송했어요");
 
     private final String template;
