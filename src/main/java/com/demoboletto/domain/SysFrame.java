23a5d994d4356cb31676a33f06db85ff83abe123
migusdn <migusdn@gmail.com>
2024-11-20 21:00:19 +0900

    feat: Add System Frame Entity

---
+++ src/main/java/com/demoboletto/domain/SysFrame.java
@@ -0,0 +1,31 @@
+package com.demoboletto.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "sys_frame")
+@NoArgsConstructor
+@Getter
+public class SysFrame {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "frame_code")
+    private String frameCode;
+
+    @Column(name = "frame_name")
+    private String frameName;
+
+    @Column(name = "frame_url")
+    private String frameUrl;
+
+    @Column(name = "description")
+    private String description;
+
+}
