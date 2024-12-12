6c8d3656aa793e2be73be89662c2488b026e4a1c
migusdn <migusdn@gmail.com>
2024-12-13 04:46:25 +0900

    feat: exception handler가 실제 error 코드 반영하도록 수정

--- src/main/java/com/demoboletto/exception/GlobalExceptionHandler.java
+++ src/main/java/com/demoboletto/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.demoboletto.exception;
 
 import com.demoboletto.dto.global.ResponseDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -20,6 +22,7 @@ public class GlobalExceptionHandler {
         log.error("handleHttpMessageNotReadableException() in GlobalExceptionHandler throw HttpMessageNotReadableException : {}", e.getMessage());
         return ResponseDto.fail(new CommonException(ErrorCode.BAD_REQUEST_JSON));
     }
+
     // 지원되지 않는 HTTP 메소드를 사용할 때 발생하는 예외
     @ExceptionHandler(value = {NoHandlerFoundException.class, HttpRequestMethodNotSupportedException.class})
     public ResponseDto<?> handleNoPageFoundException(Exception e) {
@@ -36,30 +39,33 @@ public class GlobalExceptionHandler {
 
     // 메소드의 인자 타입이 일치하지 않을 때 발생하는 예외
     @ExceptionHandler(value = {MethodArgumentTypeMismatchException.class})
-    public ResponseDto<?> handleArgumentNotValidException(MethodArgumentTypeMismatchException e) {
+    public ResponseEntity<?> handleArgumentNotValidException(MethodArgumentTypeMismatchException e) {
         log.error("handleArgumentNotValidException() in GlobalExceptionHandler throw MethodArgumentTypeMismatchException : {}", e.getMessage());
-        return ResponseDto.fail(e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ResponseDto.fail(e));
     }
 
     // 필수 파라미터가 누락되었을 때 발생하는 예외
     @ExceptionHandler(value = {MissingServletRequestParameterException.class})
-    public ResponseDto<?> handleArgumentNotValidException(MissingServletRequestParameterException e) {
+    public ResponseEntity<?> handleArgumentNotValidException(MissingServletRequestParameterException e) {
         log.error("handleArgumentNotValidException() in GlobalExceptionHandler throw MethodArgumentNotValidException : {}", e.getMessage());
-        return ResponseDto.fail(e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ResponseDto.fail(e));
     }
 
     // 개발자가 직접 정의한 예외
     @ExceptionHandler(value = {CommonException.class})
-    public ResponseDto<?> handleApiException(CommonException e) {
+    public ResponseEntity<?> handleApiException(CommonException e) {
         log.error("handleApiException() in GlobalExceptionHandler throw CommonException : {}", e.getMessage());
-        return ResponseDto.fail(e);
+        return ResponseEntity.status(e.getErrorCode().getHttpStatus())
+                .body(ResponseDto.fail(e));
     }
 
     // 서버, DB 예외
     @ExceptionHandler(value = {Exception.class})
-    public ResponseDto<?> handleException(Exception e) {
+    public ResponseEntity<?> handleException(Exception e) {
         log.error("handleException() in GlobalExceptionHandler throw Exception : {}", e.getMessage());
-        e.printStackTrace();
-        return ResponseDto.fail(new CommonException(ErrorCode.INTERNAL_SERVER_ERROR));
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ResponseDto.fail(new CommonException(ErrorCode.INTERNAL_SERVER_ERROR)));
     }
 }
